a357c63081941aa62d51eebc0c30fbcaaa1e4778
Carlos Renato <64719682+car-lo-s@users.noreply.github.com>
2023-06-12 12:48:20 -0300

    Add files via upload

---
+++ tcc-telas/produtos.js
@@ -0,0 +1,23 @@
+var dado = [
+    {
+        nome:'cafe',
+        quantidade:'200',
+        tipo:'consumivel',
+        fornecedor:'carlos',
+        descricao:'teste teste teste teste'
+    },
+    {
+        nome:'açucar',
+        quantidade:'50',
+        tipo:'consumivel',
+        fornecedor:'carlos',
+        descricao:'teste teste teste teste'
+    },
+    {
+        nome:'cafe',
+        quantidade:'200',
+        tipo:'consumivel',
+        fornecedor:'carlos',
+        descricao:'teste teste teste teste'
+    }
+]
---
+++ tcc-telas/script.js
@@ -0,0 +1,3 @@
+var elementoOriginal = document.querySelector('.exemplo table tbody tr')
+var clone = elementoOriginal.cloneNode(true)
+console.log(clone)
